10f3f277f9abf2e022e6d3c4fc88367d00d11fcf
Flappify <creative.flappify@gmail.com>
2019-10-22 18:32:20 +0530

    22102019

--- flappify.js
+++ flappify.js
@@ -1,29 +1,37 @@
-document.getElementById('paymentclick').addEventListener('click',pay);
-
 function pay(){
-    var options = {
-        "key": "rzp_test_RDEf4OFlF6k0Jw",
-        "amount": 2000, // Example: 2000 paise = INR 20
-        "name": "MERCHANT name",
-        "description": "description",
-        "image": "img/logo.png",// COMPANY LOGO
-        "handler": function (response) {
-            console.log(response);
-            // AFTER TRANSACTION IS COMPLETE YOU WILL GET THE RESPONSE HERE.
-        },
-        "prefill": {
-            "name": "ABC", // pass customer name
-            "email": 'A@A.COM',// customer email
-            "contact": '+919123456780' //customer phone no.
-        },
-        "notes": {
-            "address": "address" //customer address 
-        },
-        "theme": {
-            "color": "#A31CE3" // screen color
-        }
-    };
-    console.log(options);
-    var propay = new Razorpay(options);
-    propay.open();
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var phone=document.getElementById('phone').value;
+    var address=document.getElementById('address').value;
+    var amount=document.getElementById('amount').value;
+    if(amount>0 && amount!=null)
+    {
+        var options = {
+            "key": "rzp_test_RDEf4OFlF6k0Jw",
+            "amount": amount*100, // Example: 2000 paise = INR 20
+            "name": "FLAPPIFY",
+            "description": "Donation",
+            "image": "img/logo.png",// COMPANY LOGO
+            "handler": function (response) {
+                console.log("success");
+                $("#payCheck").trigger("reset");
+                window.location="/thankyou.html";
+                // AFTER TRANSACTION IS COMPLETE YOU WILL GET THE RESPONSE HERE.
+            },
+            "prefill": {
+                "name":name, // pass customer name
+                "email": email,// customer email
+                "contact": phone,//customer phone no.
+            },
+            "notes": {
+                "address": address, //customer address 
+            },
+            "theme": {
+                "color": "#A31CE3" // screen color
+            }
+        };
+        console.log(options);
+        var propay = new Razorpay(options);
+        propay.open();
+    }
 }
